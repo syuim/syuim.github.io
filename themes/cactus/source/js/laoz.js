1a4df4cc455a9aa89b0e8e5164801052da0d5b09
Oldchow <oldchow.zsh@gmail.com>
2023-10-08 22:41:54 +0800

    memo:dark mode

--- themes/cactus/source/js/laoz.js
+++ themes/cactus/source/js/laoz.js
@@ -3,36 +3,51 @@
     // 判断是否是夜间（假设夜间为晚上 8 点到早上 6 点）
     var isNight = currentTime < 9 || currentTime >= 18;
     // 根据时间设置夜间模式
-    if (!isNight) {
-        document.body.classList.add('dark-mode-bg');
-        document.body.classList.add('dark-mode-color');
-        document.querySelectorAll('.highlight').forEach(item=>item.classList.add('highlight-dark-mode'))
+    if (isNight) {
 
-        document.querySelector('.content').querySelector
-        
-        
-        .classList.add('.dark-mode-color');
-
-        document.a.classList.add('.dark-mode-color');
-        document.h1.classList.add('.dark-mode-color');
-        document.h2.classList.add('.dark-mode-color');
-        document.h3.classList.add('.dark-mode-color');
-        document.h4.classList.add('.dark-mode-color');
-        document.h5.classList.add('.dark-mode-color');
-        document.h6.classList.add('.dark-mode-color');
-        // document.getElementById('footer-post').classList.add('footer-post-dark-mode')
-        // document.getElementById('nav-footer').classList.add('footer-post-dark-mode2')
-        // document.getElementById('share-footer').classList.add('footer-post-dark-mode2')
-        // document.getElementById('toc-footer').classList.add('footer-post-dark-mode2')
-        // document.querySelectorAll('.highlight').forEach(item=>item.classList.add('highlight-dark-mode'))
+        document.documentElement.style.setProperty('--color-background', '#1d1f21');
+        document.documentElement.style.setProperty('--color-footer-mobile-1', '#212326');
+        document.documentElement.style.setProperty('--color-footer-mobile-2', '#323539');
+        document.documentElement.style.setProperty('--color-background-code', '#212326');
+        document.documentElement.style.setProperty('--color-border', '#908d8d');
+        document.documentElement.style.setProperty('--color-scrollbar', '#999');
+        document.documentElement.style.setProperty('--color-scrollbar-dd', '#7a7a7a');
+        document.documentElement.style.setProperty('--color-meta', '#908d8d');
+        document.documentElement.style.setProperty('--color-meta-dd', '#747070');
+        document.documentElement.style.setProperty('--color-meta-20', '#a6a4a4');
+        document.documentElement.style.setProperty('--color-meta-code-20', '#b1afaf');
+        document.documentElement.style.setProperty('--color-link', 'rgba(212, 128, 170, 1)');
+        document.documentElement.style.setProperty('--color-text', '#c9cacc');
+        document.documentElement.style.setProperty('--color-accent-3', '#cccccc');
+        document.documentElement.style.setProperty('--color-accent-2', '#eeeeee');
+        document.documentElement.style.setProperty('--color-accent-1', '#2bbc8a');
+        document.documentElement.style.setProperty('--color-quote', '#ccffb6'); 
+        document.documentElement.style.setProperty('--highlight', '\'@import "_highlight/rainbow"\'');
     } else {
-        document.body.classList.remove('dark-mode');
+        document.documentElement.style.removeProperty('--color-background')
+        document.documentElement.style.removeProperty('--color-footer-mobile-1');
+        document.documentElement.style.removeProperty('--color-footer-mobile-2');
+        document.documentElement.style.removeProperty('--color-background-code');
+        document.documentElement.style.removeProperty('--color-border');
+        document.documentElement.style.removeProperty('--color-scrollbar');
+        document.documentElement.style.removeProperty('--color-scrollbar-dd');
+        document.documentElement.style.removeProperty('--color-meta');
+        document.documentElement.style.removeProperty('--color-meta-dd');
+        document.documentElement.style.removeProperty('--color-meta-20');
+        document.documentElement.style.removeProperty('--color-meta-code-20');
+        document.documentElement.style.removeProperty('--color-link');
+        document.documentElement.style.removeProperty('--color-text');
+        document.documentElement.style.removeProperty('--color-accent-3');
+        document.documentElement.style.removeProperty('--color-accent-2');
+        document.documentElement.style.removeProperty('--color-accent-1');
+        document.documentElement.style.removeProperty('--color-quote'); 
+        document.documentElement.style.removeProperty('--highlight');
     }
    
 //     html
 //   margin: 0
 //   padding: 0
 //   height: 100%
-//   border-top: 2px solid $color-text
+//   border-top: 2px solid var(--color-text)
 //   -webkit-text-size-adjust: 100%
 //   -ms-text-size-adjust: 100%
