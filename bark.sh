3bb56d0f40b5fd660c5bf6ff9ad2bd51a21b7adf
Laoz <laoz.org@gmail.com>
2024-01-15 16:54:06 +0800

    :memo: change bark memo

--- bark.sh
+++ bark.sh
@@ -1,1 +1,1 @@
-curl https://bark.laoz.org/5iNejrxa7yHp29cAMTYiTi/✅BLOG-博客发布完成
+curl https://bark.laoz.org/5iNejrxa7yHp29cAMTYiTi/Laoz's Blog/✅BLOG-博客发布完成?automaticallyCopy=1&copy=https://laoz.org&url=https://laoz.org&isArchive=1&group=Bark&icon=https://laoz.org/images/logo.jpg
